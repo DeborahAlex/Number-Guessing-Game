0dbbc98acc1f25ad37ba4496797ae0f2492d3686
Deborah Alex <74654120+DeborahAlex@users.noreply.github.com>
2022-04-27 13:55:27 +0530

    Delete cs_091_103_120 directory

--- cs_091_103_120/src/main/java/com/dao/ILoginDao.java
+++
@@ -1,7 +0,0 @@
-package com.dao;
-
-public interface ILoginDao {
-	boolean validateUser(String username, String password);
-	String checkType(String username, String password);
-	void InsertUser(String username, String password, String Type);
-}
